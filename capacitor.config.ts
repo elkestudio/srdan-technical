c59087156af2ec80c7ea60baa6de5a03f65f3645
Srdan Topalovic <srdan@elke.studio>
2025-08-28 21:38:45 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'srdan-technical',
+  webDir: 'www'
+};
+
+export default config;
